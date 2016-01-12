da828276b245c378569756d000fb32a7e1804703
Jesper Hermansson <petsson94@hotmail.com>
2016-01-12 12:34:07 +0100

    0.4

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,16 @@
 var main = function() {
-   $('#sidetoggle').on('click', function() {
-      $('#sidebar').toggle('slide', 'left', 'slow');
-   });
-}
+ $('#sidetoggle').click(function() {
+   $('#sidebar').animate({
+     margin-left: "0px"
+   }, 200);
+ });
 
-$(document).ready(main);
+
+ $('#sidetoggle').click(function() {
+   $('#sidebar').animate({
+     margin-left: "-285px"
+   }, 200);
+ });
+};
+
+$(document).ready(main)
